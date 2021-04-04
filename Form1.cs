9979234a9f6d2aeeaf62a4bf1c566b7b4a99abef
Rene Gonzalez <rene_glez1992@hotmail.com>
2021-04-03 19:00:53 -0700

    se  borran using no utilizados y se agrega diccionario de datos

--- Form1.cs
+++ Form1.cs
@@ -1,13 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-
+/// <summary>
+/// Lenguaje de programacion III
+/// Autor: Rene Gonzalez Rodriguez
+/// Maestro: Aarón I. Salazar
+/// </summary>
 namespace AppWinRestaurant
 {
     public partial class Form1 : Form
